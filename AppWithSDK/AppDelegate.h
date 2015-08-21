5d2f665827d6274f45a38efe7ed3da9e8a568494
Stainsby, Hayden <hayden.stainsby@here.com>
2015-08-21 15:10:59 +0200

    Initial Commit

---
+++ AppWithSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppWithSDK
+//
+//  Created by Stainsby, Hayden on 21/08/15.
+//  Copyright (c) 2015 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppWithSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppWithSDK
+//
+//  Created by Stainsby, Hayden on 21/08/15.
+//  Copyright (c) 2015 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
